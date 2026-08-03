5fd779c720794d670c417e0f8ecfd7437ca34ab0
Vipin Kumawat <kumawatvipin066@gmail.com>
2026-08-03 22:51:40 +0530

    project config service

---
+++ app/core/services/project_config_service.py
@@ -0,0 +1,37 @@
+from ..schemas.project_config_schema import ProjectConfigCreateSchema, ProjectConfigGetSchema, ProjectConfigUpdateSchema
+from ..repos.project_config_repo import ProjectConfigRepo
+from app.utils.logger import logger
+import uuid
+
+
+class ProjectConfigService:
+    def __init__(self, org_id: str, project_id: uuid.UUID):
+        self._repo = ProjectConfigRepo(org_id, project_id)
+
+    async def create(self, p: ProjectConfigCreateSchema) -> ProjectConfigGetSchema:
+        try:
+            return await self._repo.create(p)
+        except Exception as e:
+            logger.error({"message": "Failed to create project config", "error": str(e)})
+            raise e
+
+    async def get(self, config_id: uuid.UUID) -> ProjectConfigGetSchema | None:
+        try:
+            return await self._repo.get(config_id)
+        except Exception as e:
+            logger.error({"message": "Failed to get project config", "error": str(e)})
+            raise e
+
+    async def update(self, config_id: uuid.UUID, u: ProjectConfigUpdateSchema) -> ProjectConfigGetSchema:
+        try:
+            return await self._repo.update(config_id, u)
+        except Exception as e:
+            logger.error({"message": "Failed to update project config", "error": str(e)})
+            raise e
+
+    async def delete(self, config_id: uuid.UUID) -> bool:
+        try:
+            return await self._repo.delete(config_id)
+        except Exception as e:
+            logger.error({"message": "Failed to delete project config", "error": str(e)})
+            raise e
